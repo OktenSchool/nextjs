71e2a364bee445666d2307df66104c6e7b639c29
GrayHead <ss.zhuravlov@gmail.com>
2024-12-27 19:21:33 +0200

    pass data bw pages

--- src/app/(public)/users/[id]/page.tsx
+++ src/app/(public)/users/[id]/page.tsx
@@ -1,25 +1,36 @@
 import {FC} from "react";
 import {Metadata} from "next";
+import {SearchParams} from "next/dist/server/request/search-params";
+import {IUser} from "@/models/IUser";
 
 type Props = {
-    params: { id: string };
+    params: Promise<{ id: string }>;
+    searchParams: Promise<SearchParams>;
+
 }
 export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
     const {id} = await params;
-
     return {
         title: 'User page  title' + id,
     }
 }
 
-const UserPage: FC<Props> = async ({params}) => {
+const UserPage: FC<Props> = async ({searchParams}) => {
 
-    const {id} = await params;
+
+    const {data} = await searchParams;
+    let obj = null;
+    if (typeof data === "string") {
+        obj = JSON.parse(data) as IUser;
+    }
 
 
     return (
         <div>
-            user page content {id}
+            {
+                obj && <>user page content {obj.id} {obj.name}</>
+
+            }
         </div>
     );
 };
