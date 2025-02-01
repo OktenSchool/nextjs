2cb396ee82f4578292eb6f4ea2a84f4beca3fd3a
GrayHead <ss.zhuravlov@gmail.com>
2025-02-01 18:19:22 +0200

    server actions and mutations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
 import styles from "./page.module.css";
 import Form from "next/form";
+import {getMeals, saveMeal} from "@/server-actions/serverActions";
 
-export default function Home() {
+export default async function Home() {
+
+    const mealsArray = await getMeals();
+
     return (
         <div className={styles.page}>
 
-            {/*<form action="/submit">*/}
-            {/*    <input type="text" name={'name'} placeholder={'login'}/>*/}
-            {/*    <input type="text" name={'password'} placeholder={'password'}/>*/}
-            {/*    <button>submit</button>*/}
 
-            {/*</form>*/}
-
-            <Form action={'/submit'}>
-                <input type="text" name={'name'}/>
+            <Form action={saveMeal}>
+                <input type="text" name={'title'}/>
                 <button>send</button>
-
-
             </Form>
+
+            <div className={'meals'}>
+                {
+                    mealsArray.map(meal => (<div key={meal.id}>{meal.title}</div>))
+
+                }
+
+            </div>
 
         </div>
     );
