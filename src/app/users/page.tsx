2eed9d37d84a801bc5752eb4bb81aeb2131bb288
GrayHead <ss.zhuravlov@gmail.com>
2025-02-05 15:12:43 +0200

    ISR

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,10 +1,12 @@
 const UsersPage = async () => {
-    const users = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await fetch('https://jsonplaceholder.typicode.com/users',{cache: 'no-cache'})
         .then(value => value.json());
 
 
     return (
         <div>
+
+            <h2>{Date.now()}</h2>
 
             {users.map((user: { id: number, name: string }) => (<div key={user.id}>
                 {user.name}
