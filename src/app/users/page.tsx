edfa9817754ddf5d0843e61005f6d321e495380c
GrayHead <ss.zhuravlov@gmail.com>
2025-02-05 14:45:40 +0200

    SSG

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,10 +1,16 @@
-const Users = async () => {
-
+const UsersPage = async () => {
     const users = await fetch('https://jsonplaceholder.typicode.com/users')
         .then(value => value.json());
-    console.log(users);
+
+
     return (
-        <></>
+        <div>
+
+            {users.map((user: { id: number, name: string }) => (<div key={user.id}>
+                {user.name}
+            </div>))}
+
+        </div>
     )
 }
-export default Users;
+export default UsersPage;
